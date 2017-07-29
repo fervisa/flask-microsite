2045a73311527b53d555bf35206c77c139d8a1e8
Fernando Villalobos <fernando.visa@gmail.com>
2017-07-29 12:36:59 -0500

    Add cli command to export contacts to CSV file

--- microsite/microsite.py
+++ microsite/microsite.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
-from microsite.db import get_db
+from microsite.db import get_db, query_db
+import csv
 
 app = Flask(__name__)
 
@@ -17,6 +18,15 @@ def contact():
                 [request.form['email'], request.form['message']])
         db.commit()
         return 'Thanks for reaching out to us, {}'.format(request.form['email'])
+
+@app.cli.command('export_contacts')
+def export_contacts():
+    contacts = query_db('select * from contacts')
+    with open('contacts.csv', 'w') as outfile:
+        writer = csv.writer(outfile)
+        for contact in contacts:
+            writer.writerow([contact['email'], contact['message']])
+        print('Contacts have been successfully exported')
 
 @app.teardown_appcontext
 def close_connection(exception):
